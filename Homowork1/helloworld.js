8c4c7aca43bb9b52ea22edd23d3505be79d91eb7
Henry <80731107+HengWang20001029@users.noreply.github.com>
2022-06-30 11:14:55 +0800

    put0

---
+++ Homowork1/helloworld.js
@@ -0,0 +1,16 @@
+console.log("Hello World000000000000")
+
+document.body.innerHTML = "Hello World"
+
+let folder0={
+    size:2000,
+    color:"red",
+
+}
+const origion_color = document.getElementById("world").style.color;
+function change_color(){
+    document.getElementById("world").style.color="red";
+}
+function reset_color(){
+    document.getElementById("world").style.color= "aquamarine";
+}
